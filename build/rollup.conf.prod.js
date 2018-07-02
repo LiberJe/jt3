52466d6b816aa8da03992fa348c8fe5f5df702d3
LiberJe <jier15@163.com>
2018-07-02 22:29:08 +0800

    add rollup

---
+++ build/rollup.conf.prod.js
@@ -0,0 +1,8 @@
+
+export default {
+  input: 'lib/index.js',
+  output: {
+    file: 'dist/jt3.js',
+    format: 'cjs'
+  }
+}
---
+++ dist/jt3.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const test = 1;
+console.log(test);
+
+module.exports = test;
